195f50e0877f151f62d206467ea899e78752d63d
reesericci <reesericci@nyctech.cloud>
2021-08-24 21:04:53 -0500

    fixed rickroll

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 var videoModal = new bootstrap.Modal(document.getElementById('videoModal'));
 function autoPlayYouTubeModal(){
       var theModal = $('#videoModal'),
-      videoSRC = "https://peervideo.club/videos/embed/f0cd16c8-dbc2-4774-a97f-b5507c3c68a0?loop=1&autoplay=1&title=0&warningTitle=0&controls=0&peertubeLink=0",
-      videoSRCauto = videoSRC+"?autoplay=1" ;
+      videoSRC = "https://rickroll69.netlify.app/rickroll.mp4",
+      videoSRCauto = videoSRC+"?autoplay=1&controls=0" ;
       $("#modal-iframe").attr('src', videoSRCauto);
       $("#modal-close").click(function () {
           $("#modal-iframe").attr('src', videoSRC);
